c399dbd401c3b37e681e08c6b553f915181ae388
Rimfis <dimitri.rim@yandex.ru>
2025-05-11 17:05:25 +0300

    feat: add types user, metod, status code

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export enum Method {
+    GET='GET',
+    POST='POST',
+    PUT='PUT',
+    DELETE='DELETE',
+}
+
+export enum HttpStatusCode {
+    OK = 200,
+    CreatedRecord = 201,
+    DeletedRecord = 204,
+    NotFound = 404,
+    InvalidUuid = 400,
+    InternalServerError = 500,
+}
+
+export type User = {
+    id: string;
+    username: string;
+    age: number;
+    hobbies: string[];
+}
